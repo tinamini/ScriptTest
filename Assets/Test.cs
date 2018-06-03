2218346373d5c7ca08620394323de88b65688c34
tinamini <atsushi.matsui.7865@gmail.com>
2018-06-03 20:59:23 +0900

    ScriptTest Project Initial-Commit
    GameSceneを作成し、GameObjectにTestスクリプトをアタッチした状態でのコミット。

---
+++ Assets/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start()
+	{
+
+	}
+
+	/// <summary>
+	/// Update is called once per frame
+	/// </summary>
+	void Update()
+	{
+
+	}
+}
