e5f49682ff133adc5207f3790bf969cde90f90d7
tinamini <atsushi.matsui.7865@gmail.com>
2018-06-03 22:18:58 +0900

    Lesson4　9.3 Bossクラスの追加
    攻撃・防御ができるBossクラスを追加。
    https://techacademy.jp/my/unity/c-script#chapter-9-3

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+#region Test Class
 public class Test : MonoBehaviour
 {
 	/// <summary>
@@ -9,7 +10,12 @@ public class Test : MonoBehaviour
 	/// </summary>
 	void Start()
 	{
+		var lastBoss = new Boss();
 
+		// 攻撃
+		lastBoss.Attack();
+		// 防御
+		lastBoss.Defence(3);
 	}
 
 	/// <summary>
@@ -20,3 +26,44 @@ public class Test : MonoBehaviour
 
 	}
 }
+#endregion
+
+#region Boss Class
+/// <summary>
+/// Boss クラス
+/// </summary>
+public class Boss
+{
+	#region Field
+	/// <summary>
+	/// 体力
+	/// </summary>
+	private int hp = 100;
+
+	/// <summary>
+	/// 攻撃力
+	/// </summary>
+	private int power = 25;
+	#endregion
+
+	#region Public Method
+	/// <summary>
+	/// 攻撃する
+	/// </summary>
+	public void Attack()
+	{
+		Debug.Log(this.power + "のダメージを与えた");
+	}
+
+	/// <summary>
+	/// 防御する。
+	/// </summary>
+	/// <param name="damage">ダメージ</param>
+	public void Defence(int damage)
+	{
+		this.hp -= damage;
+		Debug.Log(damage + "のダメージを受けた");
+	}
+	#endregion
+}
+#endregion
